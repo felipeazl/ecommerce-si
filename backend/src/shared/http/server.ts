4c197d33e85edc10226acfdf3b9de5a5677af495
almakad <carls-f@live.com>
2021-03-25 16:40:41 -0300

    refactor: update files

--- backend/src/shared/http/server.ts
+++ backend/src/shared/http/server.ts
@@ -1,23 +1,27 @@
 import express, { NextFunction, Request, Response } from 'express';
-import AppError from '../errors/AppError.js';
+import AppError from '../errors/AppError';
 import 'dotenv/config.js';
+import cors from 'cors';
+import { errors } from 'celebrate';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use((err: Error, req: Request, res:Response, _: NextFunction) => {
+app.use(errors());
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
       status: 'error',
-      message: err.message
+      message: err.message,
     });
   }
   return res.status(500).json({
     error: 'error',
-    message: 'Internal Server Error'
-  })
-})
+    message: 'Internal Server Error',
+  });
+});
 
-
-app.listen(process.env.PORT || 3000, () => console.log('Server running 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('Server running 3000'));
