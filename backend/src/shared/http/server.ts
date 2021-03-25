c60eed3462cbdb712e895dbfa0a8576f7c87737d
almakad <carls-f@live.com>
2021-03-25 17:07:00 -0300

    feat: create route to createUsers

--- backend/src/shared/http/server.ts
+++ backend/src/shared/http/server.ts
@@ -1,16 +1,19 @@
 import 'reflect-metadata';
-import 'dotenv/config.js';
-import '../typeorm';
+import 'dotenv/config';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import AppError from '../errors/AppError';
-import express, { NextFunction, Request, Response } from 'express';
-
+import '../typeorm';
+import routes from '../routes';
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
+app.use(routes);
 app.use(errors());
 
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
@@ -20,10 +23,11 @@ app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
       message: err.message,
     });
   }
+
   return res.status(500).json({
-    error: 'error',
-    message: 'Internal Server Error',
+    status: 'error',
+    message: 'Internal server error',
   });
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Server running 3000'));
+app.listen(3000, () => console.log('Server running on port 3333!'));
