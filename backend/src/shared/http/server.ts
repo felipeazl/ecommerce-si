d3fa82ad4d8d4f5753d20b78db615a75bdaf5cdb
almakad <carls-f@live.com>
2021-03-25 16:49:19 -0300

    feat: add User Entity

--- backend/src/shared/http/server.ts
+++ backend/src/shared/http/server.ts
@@ -1,8 +1,10 @@
-import express, { NextFunction, Request, Response } from 'express';
-import AppError from '../errors/AppError';
+import 'reflect-metadata';
 import 'dotenv/config.js';
+import '../typeorm';
 import cors from 'cors';
 import { errors } from 'celebrate';
+import AppError from '../errors/AppError';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 
