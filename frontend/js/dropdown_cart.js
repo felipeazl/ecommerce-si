6980a6abb145a7da4492e7ae802ba2fbd890421d
almakad <carls-f@live.com>
2021-05-11 14:01:36 -0300

    feat:

--- frontend/js/dropdown_cart.js
+++ frontend/js/dropdown_cart.js
@@ -1,6 +1,36 @@
 const dropdown_cart = document.getElementById('dropdown-cart');
+let cartProduct = document.getElementsByClassName('cart-product')
 
 
 function showDropdownCart() {
   dropdown_cart.classList.toggle('show-dropdown-cart');
 }
+
+
+let totalProdutcs = JSON.parse(localStorage.getItem('order_id'))
+let userId = sessionStorage.getItem('client_id')
+
+let totalValueIdOne = 0
+let totalProds = Array(16)
+let showTotal = {}
+
+function generateCartWithProdutcts() {
+
+  for (let produto of totalProdutcs) {
+    if (produto.client_id === userId) {
+
+      for (let i = 0; i < totalProds.length; i++) {
+        if (produto.desc.id === `${i + 1}`) {
+          showTotal = produto
+          showTotal.desc.quantidade += produto.desc.quantidade
+          totalValueIdOne += Number(produto.desc.prod_price)
+        }
+      }
+
+    }
+  }
+
+  console.log(showTotal)
+}
+
+generateCartWithProdutcts()
