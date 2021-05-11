b0bb6f0d5fbafb9de1730935cb9045130d7b6458
almakad <carls-f@live.com>
2021-05-10 23:29:08 -0300

    feat:

--- frontend/js/dropdown_cart.js
+++ frontend/js/dropdown_cart.js
@@ -1,6 +1,6 @@
 const dropdown_cart = document.getElementById('dropdown-cart');
 
+
 function showDropdownCart() {
   dropdown_cart.classList.toggle('show-dropdown-cart');
 }
-
--- frontend/js/shopping.js
+++ frontend/js/shopping.js
@@ -1,12 +1,17 @@
 let container = document.getElementById('container-produtos')
 let listPedidos = document.getElementById('cart')
 let dropDownProdList = document.getElementById('inside-cart')
+let totalBuy = document.getElementById('totalBuy')
 
 try {
   let total = `${JSON.parse(localStorage.getItem('order_id')).length}`
+  
   listPedidos.setAttribute('data-totalitems', total)
   
   dropDownProdList.setAttribute('data-totalitems', total)
+
+  totalBuy.textContent = `R$ ${localStorage.getItem('totalValue')}`
+
 } catch (error) {
   console.error(error)
 }
@@ -57,7 +62,7 @@ async function shopping(index) {
     desc: {
       'id': produto[index].id,
       'prod_title': produto[index].title,
-      'prod_price': produto[index].price,
+      'prod_price': produto[index].price.replaceAll(',', '.'),
       'quantidade': 1,
     }
   }
@@ -80,5 +85,24 @@ function calcularCompra(totalPedidos) {
   
   let totalProduts = JSON.parse(localStorage.getItem('order_id'))
   dropDownProdList.setAttribute('data-totalitems', totalProduts.length)
+
+  
+let totalItems = JSON.parse(localStorage.getItem('order_id'))
+
+let totalValue = 0
+for (const item of totalItems) {
+  if (item.client_id === undefined || item.client_id === null) {
+    try {
+      item.client_id = sessionStorage.getItem('client_id')
+      
+    } catch (error) {
+      
+    }
+  }
+  totalValue += Number(item.desc.prod_price)
+}
+  totalBuy.textContent = `R$ ${totalValue.toFixed(2)}`
+  
+  localStorage.setItem('totalValue', `${totalValue.toFixed(2)}`)
 
 }
